9890e646647d7053dc03fd8e65d1121ff4171df6
WMark5477 <mwachowiak5477@gmail.com>
2025-04-01 17:28:09 +0200

    Added exit builtin

--- src/main.cs
+++ src/main.cs
@@ -5,8 +5,24 @@ bool Running = false;
 Running = true;
 
 while (Running) {
-   Console.Write("$ ");
-   var command = Console.ReadLine();
-   Console.WriteLine($"{command}: command not found");
+    Console.Write("$ ");
+
+    var input = Console.ReadLine().Split(" ");
+    var command = input[0];
+    var parameters = input[1..];
+
+
+    switch (command)
+    {
+        case "exit":
+            if (parameters.Length != 0 && parameters[0] == "0") 
+                Running = false;
+            else
+                Console.WriteLine("exit: invalid parameter");
+            break;
+        default:
+            Console.WriteLine($"{command}: command not found");
+            break;
+    }
 }
 
