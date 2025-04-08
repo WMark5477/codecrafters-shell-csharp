f7d72b0d1ec6b858a5dd952e8783c370560c68ee
WMark5477 <mwachowiak5477@gmail.com>
2025-04-08 21:27:30 +0200

    Minor refactorization

--- src/main.cs
+++ src/main.cs
@@ -1,76 +1,28 @@
 using System.Net;
 using System.Net.Sockets;
 
-static bool IsExecutable(string filePath)
-{
-    if (OperatingSystem.IsWindows())
-    {
-        string ext = Path.GetExtension(filePath).ToLower();
-        return ext == ".exe" || ext == ".bat" || ext == ".cmd";
-    }
-    else
-        return new FileInfo(filePath).Exists && (new FileInfo(filePath).Attributes & FileAttributes.Directory) == 0;
-}
-
-static string? FindCommandInPath(string command)
-{
-    var paths = Environment.GetEnvironmentVariable("PATH")?.Split(Path.PathSeparator) ?? Array.Empty<string>();
-
-    foreach (var path in paths)
-    {
-        string fullPath = Path.Combine(path, command);
-        if (File.Exists(fullPath) && IsExecutable(fullPath))
-            return fullPath;
-    }
-
-    return null;
-}
-
-bool Running = false;
-Running = true;
-
-string[] builtins = { "exit", "echo", "type" };
-
-while (Running)
+while (true)
 {
     Console.Write("$ ");
 
     var input = Console.ReadLine()?.Split(' ', StringSplitOptions.RemoveEmptyEntries);
     if (input == null || input.Length == 0) continue;
 
-    var command = input[0];
+    var command = input[0].ToLower(); ;
     var parameters = input.Length > 1 ? input[1..] : Array.Empty<string>();
-    
+
     switch (command)
     {
-        case "exit":
-            if (parameters.Length != 0 && parameters[0] == "0") 
-                Running = false;
-            else
-                Console.WriteLine("exit: invalid parameter");
-            break;
+        case Builtins.Exit:
+            Builtins.ExitCommand(parameters); break;
 
-        case "echo":
-            Console.WriteLine(string.Join(" ", parameters));
-            break;
+        case Builtins.Echo:
+            Builtins.EchoCommand(parameters); break;
+            
+        case Builtins.Type:
+            Builtins.TypeCommand(parameters); break;
 
-        case "type":
-            if (parameters.Length != 0)
-            {
-                if (builtins.Contains(parameters[0]))
-                    Console.WriteLine($"{parameters[0]} is a shell builtin");
-
-                else if (FindCommandInPath(parameters[0]) is string path)
-                    Console.WriteLine($"{parameters[0]} is {path}");
-
-                else
-                    Console.WriteLine($"{parameters[0]}: not found");
-            }
-            else
-                Console.WriteLine("type: missing parameter");
-            break;
         default:
-            Console.WriteLine($"{command}: command not found");
-            break;
+            Console.WriteLine($"{command}: command not found"); break;
     }
 }
